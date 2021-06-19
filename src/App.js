437031a295276b49626c0666178cb3aaa04fc1d5
facorda <acorda.fred@gmail.com>
2021-06-19 17:21:02 -0400

    2.2.5 Built Navigation Component.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import { GlobalStyle } from "./utils";
 import { Button } from "./components/Button";
 import { Logo } from "./components/Logo";
+import { Header, Nav, NavItem } from "./components/Nav";
 
 const App = () => {
   return (
     <main>
       <GlobalStyle />
-      <Logo />
-      <br />
-      <br />
-      <Logo variant="secondary" href="#" />
-      <br />
-      <br />
+      <Header>
+        <Logo />
+        <Nav>
+          <NavItem href="#">Shop</NavItem>
+          <NavItem href="#">Blog</NavItem>
+          <NavItem href="#">Location</NavItem>
+          <NavItem href="#">Contact</NavItem>
+        </Nav>
+      </Header>
+
+      <Header variant="right">
+        <Logo variant="secondary" href="#" />
+        <Nav>
+          <NavItem href="#">Shop</NavItem>
+          <NavItem href="#">Blog</NavItem>
+          <NavItem href="#">Location</NavItem>
+          <NavItem href="#">Contact</NavItem>
+        </Nav>
+      </Header>
+
       <Button href="http://google.com">This is a button with a link</Button>
       <br />
       <br />
